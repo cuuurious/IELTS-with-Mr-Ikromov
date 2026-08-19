833274410a9875a84edac05d08dbb9cbd398ebce
cuuurious <mavludamamazokirova7@gmail.com>
2026-08-19 14:32:05 +0500

    Fix custom domain base path

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/IELTS-with-Mr-Ikromov/',
+  base: '/',
   plugins: [react(), tailwindcss()],
 })
